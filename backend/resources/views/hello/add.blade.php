add6ec8160a41a869b3b47d35267748eb96a474f
y-keysynth <ykhojojp@gmail.com>
2021-09-06 22:32:21 +0900

    完了

--- backend/resources/views/hello/add.blade.php
+++ backend/resources/views/hello/add.blade.php
@@ -8,7 +8,7 @@
 <h1>POST通信してみる（フォーム）</h1>
  
 <form action="{{ url('/hello/result')}}" method="POST">
-    {{ csrf_field() }}
+    @csrf
     <div><textarea rows="6" name="message"></textarea></div>
     <div><input type="submit" name="add"></div>
 </form>
